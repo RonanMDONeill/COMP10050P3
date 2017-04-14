802b417c46055fea4a0998598029104f8b6bf436
RonanMDONeill <ronan.oneill@ucdconnect.ie>
2017-04-14 19:40:11 +0100

    Update main.c

--- main.c
+++ main.c
@@ -14,37 +14,42 @@ int main(){
 
 	makePlayer();
 
-	struct slot *upLeft;
+		struct slot *upLeft;
 	struct slot *upRight;
 	struct slot *downLeft;
 	struct slot *downRight;
 
-	boardSize= getBoardSize();
-
+	boardSize = getBoardSize();
 	createBoard(boardSize,&upLeft,&upRight,&downLeft,&downRight);
+	
+	struct slot gameBoard[boardSize][boardSize];
 
-	getDesiredElement(boardSize,&row,&column);
-
-	if(row >= boardSize/2){
-			if(column >= boardSize/2)
-				reachDesiredElement(row,column,downRight);
-			else
-			/*If the the required slot is closer to the down-left
-			* corner of the board the search starts from downLeft,
-			* which points to slot at position (boardSize-1, 0)*/
-				reachDesiredElement(row,column,downLeft);
-		} else {
-			/*If the the required slot is closer to the up-right
-			* corner of the board the search starts from upRight,
-			* which points to slot at position (0, boarSize -1)*/
-			if(column >= boardSize/2)
-				reachDesiredElement(row,column, upRight);
-			/*If the the required slot is closer to the up-left
-			* corner of the board the search starts from upLeft,
-			* which points to slot at position (0, 0)*/
-			else reachDesiredElement(row,column,upLeft);
+	for(i = 0; i < boardSize; i++){
+		for(j = 0; j < boardSize; j++){
+			gameBoard[i][j].row = i;
+			gameBoard[i][j].column = j;
+			gameBoard[i][j].slotType = slotAssign(boardSize);
 		}
+	}
 
+	printf("The board is as follows:\n");
+	for(int i=0;i<boardSize;i++){
+		for(int j=0;j<boardSize;j++){
+			printf("([%d][%d] %d)\t",gameBoard[i][j].row, gameBoard[i][j].column, gameBoard[i][j].slotType);
+		}
+		printf("\n");
+	}
+	
+	srand(time(NULL));
+	for(i=0;i<playerCount;i++){
+		player[i].rowp = rand()%boardSize;
+		player[i].columnP = rand()%boardSize;
+		player[i].slotType = gameBoard[player[i].rowp][player[i].columnP].slotType;
+	}
+	
+	for(i=0;i<playerCount;i++){
+		printf("%s (%d, %.2f) is on slot [%d][%d] %d\n", player[i].name, player[i].class,player[i].life,player[i].rowp,player[i].columnP, player[i].slotType);
+	}
 
 
 
